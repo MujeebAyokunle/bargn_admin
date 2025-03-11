e9069d4cfd6b6952843391a1fb7e010afc927a3d
mujeebayokunle <alabimujeebayokunle@gmail.com>
2025-03-11 10:19:27 +0100

    update

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -6,7 +6,9 @@ import HeatMap from '@/components/HeatMap'
 import LineChart from '@/components/LineChart'
 import Nav from '@/components/Nav'
 import moment from 'moment'
+import ReactMarkdown from 'react-markdown';
 import React, { useEffect, useState } from 'react'
+import { ColorSchema } from '@/helper/colorScheme'
 
 function Analytics() {
 
@@ -18,6 +20,7 @@ function Analytics() {
     const [androidPercentage, setandroidPercentage] = useState(0)
     const [iosPercentage, setiosPercentage] = useState(0)
     const [sentimentData, setsentimentData] = useState(0)
+    const [userActivityInsight, setUserActivityInsight] = useState("")
 
 
     useEffect(() => {
@@ -28,12 +31,14 @@ function Analytics() {
         const response = await FetchAnalyticsData()
         // console.log(response)
         if (!response.error) {
+            console.log(response)
             setReviews(response.appStoreReviewsData)
             setgplayRating(response.gplayRating)
             setandroidPercentage(response.androidPercentage)
             setiosPercentage(response.iosPercentage)
             setsentimentData(response.sentimentData)
             setDealsBySubCategoryCount(response?.dealsBySubCategoryCount)
+            setUserActivityInsight(response?.eventsActivityInsights)
 
             let temp = response?.dealsByCategoryCount
             let categoriesTemp = Object.keys(temp)
@@ -45,13 +50,36 @@ function Analytics() {
         }
     }
 
-    console.log(dealsBySubCategoryCount)
-
     return (
         <Nav>
             <div className='p-4'>
                 <div className="mb-6 w-full">
                     <h1 className="text-2xl text-[#0A0909] font-bold">Analytics</h1>
+
+                    {/* User activity insight */}
+                    <div className='w-full rounded-lg mt-8' >
+                        <div className='w-full rounded-lg border-2' style={{ borderColor: "rgba(0,0,0,0.05)" }} >
+                            <div className='flex' >
+                                <div className='p-8' >
+                                    {/* <p className='text-[#333] text-base font-semibold' >Android</p> */}
+                                    {/* <p className='text-black font-normal text-[16px]' >{userActivityInsight}</p> */}
+                                    <ReactMarkdown
+                                        components={{
+                                            p: ({ node, ...props }) => <p style={{ color: ColorSchema.black }} {...props} />,
+                                            strong: ({ node, ...props }) => <strong style={{ color: ColorSchema.black }} {...props} />,
+                                            li: ({ node, ...props }) => <li style={{ color: ColorSchema.black }} {...props} />,
+                                        }}
+                                    >
+                                        {userActivityInsight}
+                                    </ReactMarkdown>
+                                </div>
+                            </div>
+
+                            <div style={{ backgroundColor: "rgba(0,0,0,0.05)" }} className='p-4 rounded-bl-lg rounded-br-lg' >
+                                <p className='text-[#333] text-sm font-semibold' >AI-Generated User Activity Insights</p>
+                            </div>
+                        </div>
+                    </div>
 
                     {/* Acquisition channel and app store ranking */}
                     <div className='flex justify-between items-start w-full h-auto mt-8' >
